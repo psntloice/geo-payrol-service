8d37219c17f2917d3d215e2f40dc7f7e423ee73a
loice kamau <loice.kamau@jnam.gdc>
2024-07-09 12:17:09 +0300

    vercel

--- bd/bootstrap/app.php
+++ bd/bootstrap/app.php
@@ -13,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
-        apiPrefix: 'api',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
