e542986da1b27427cb2ea35abb7c0a838e282658
loice kamau <loice.kamau@jnam.gdc>
2024-07-09 09:55:10 +0300

    vercel

--- bd/public/index.php
+++ bd/public/index.php
@@ -1,17 +1,52 @@
 <?php
 
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 
-define('LARAVEL_START', microtime(true));
+// define('LARAVEL_START', microtime(true));
+
+// // Determine if the application is in maintenance mode...
+// if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+//     require $maintenance;
+// }
+
+// // Register the Composer autoloader...
+// require __DIR__.'/../vendor/autoload.php';
+
+// // Bootstrap Laravel and handle the request...
+// (require_once __DIR__.'/../bootstrap/app.php')
+//     ->handleRequest(Request::capture());
+
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
+
+// define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
+// // Register the Composer autoloader...
+// require __DIR__.'/../vendor/autoload.php';
+
+// // Bootstrap Laravel and handle the request...
+// (require_once __DIR__.'/../bootstrap/app.php')
+//     ->handleRequest(Request::capture());
+
+
+
+
+
+
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
