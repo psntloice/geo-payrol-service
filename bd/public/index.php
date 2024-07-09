c456191b044dd6ac7a6b4587378689f0c6b6ebfc
loice kamau <loice.kamau@jnam.gdc>
2024-07-09 09:58:52 +0300

    vercel

--- bd/public/index.php
+++ bd/public/index.php
@@ -39,6 +39,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 
 
 
+
 require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
